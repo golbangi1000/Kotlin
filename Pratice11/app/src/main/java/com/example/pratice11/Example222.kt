616a87110788f02fb10967f275b06ba6f75c8e1a
golbangi1000 <kenjames777@naver.com>
2023-07-06 21:39:03 +0900

    aa
    a

--- Pratice11/app/src/main/java/com/example/pratice11/Example222.kt
+++ Pratice11/app/src/main/java/com/example/pratice11/Example222.kt
@@ -7,21 +7,23 @@ fun main() {
     var kid = Kid("bob", 12, "남")
 
 }
-    open class User(open val name : String ,open var age : Int = 1)
+
+open class User(open val name: String, open var age: Int = 1)
 
 
-    class Kid(override val name: String, override var age: Int) : User(name, age) {
+class Kid(override val name: String, override var age: Int) : User(name, age) {
 
-        init {
-            println("이거 먼저  ")
-        }
-
-
-        var gender : String = "female"
-        constructor(name: String, age : Int, gender : String) : this(name,age){
-            this.gender = gender
-                println("부생성자")
-
-        }
+    init {
+        println("이거 먼저  ")
     }
+
+
+    var gender: String = "female"
+
+    constructor(name: String, age: Int, gender: String) : this(name, age) {
+        this.gender = gender
+        println("부생성자")
+
+    }
+}
 
